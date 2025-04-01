241400eb0c0cc7d289a48096e497cec130ecf5ca
agarbac <agarbac@deacero.com>
2025-04-01 13:13:35 -0600

    Update r1.sql

--- solutions/r1.sql
+++ solutions/r1.sql
@@ -18,8 +18,8 @@ SELECT
     vpc.region,
     vpc.categoria,
     vpc.total_ventas,
-    ROUND((vpc.total_ventas * 100.0 / tr.region_total), 1) || '%' AS porcentaje_region,
+    CAST(CAST((vpc.total_ventas * 100.0 / tr.region_total) AS NUMERIC(10,1)) AS TEXT) || '%' AS porcentaje_region,
     RANK() OVER (PARTITION BY vpc.region ORDER BY vpc.total_ventas DESC) AS ranking_region
 FROM ventas_por_region_categoria vpc
 JOIN total_region tr ON vpc.region = tr.region
-ORDER BY vpc.region, ranking_region ASC; 
+ORDER BY vpc.region, ranking_region ASC;
